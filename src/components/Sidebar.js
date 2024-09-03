2b69743b826c0e867d37710f1e031ce85336733a
Andrew Duckworth <91503262+agduck@users.noreply.github.com>
2024-09-02 19:33:36 -0500

    Styled the Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,30 +5,36 @@ import './Sidebar.css';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="sidebar-rectangle"></div>
-      <h1 className="sidebar-name">Andrew Duckworth</h1>
-      <div className="sidebar-jobs">
-        <h2>Software Developer</h2>
-        <h2>Graphic Designer</h2>
+    <div>
+      {/* Top Bar */}
+      <div className="topbar">
+        <h1 className="topbar-name">Andrew Duckworth</h1>
+        <div className="topbar-jobs">
+          <h2>Software Developer • Graphic Designer</h2>
+        </div>
       </div>
-      <nav className="sidebar-menu">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/works">Works</Link></li>
-        </ul>
-      </nav>
-      <div className="sidebar-icons">
-        <a href="https://www.linkedin.com/in/andrewgduckworth" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-          <FaLinkedin size={24} />
-        </a>
-        <a href="./andrew-duckworth-resume-720.pdf" target="_blank" rel="noopener noreferrer" aria-label="CV">
-          <FaFileAlt size={24} />
-        </a>
+
+      {/* Sidebar */}
+      <div className="sidebar">
+        <div className="sidebar-rectangle"></div>
+        <nav className="sidebar-menu">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/works">Works</Link></li>
+          </ul>
+        </nav>
+        <div className="sidebar-icons">
+          <a href="https://www.linkedin.com/in/andrewgduckworth" target="_blank" rel="noopener noreferrer" className="icon" aria-label="LinkedIn">
+            in
+          </a>
+          <a href="./andrew-duckworth-resume-720.pdf" target="_blank" rel="noopener noreferrer" className="icon" aria-label="CV">
+            cv
+          </a>
+        </div>
+        <p className="sidebar-copyright">&copy; 2024 Andrew Duckworth</p>
+        <p className="sidebar-credits">Website created <br></br> from the ground up <br></br> by Andrew Duckworth.</p>
       </div>
-      <p className="sidebar-copyright">&copy; 2024 Andrew Duckworth</p>
-      <p className="sidebar-credits">Website created from the ground up by Andrew Duckworth.</p>
     </div>
   );
 };
