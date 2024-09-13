5f7588c58deade7d4075c64260f16dc0b29466ba
Andrew Duckworth <91503262+agduck@users.noreply.github.com>
2024-09-12 22:26:01 -0500

    Implemented some more files into the Works page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaLinkedin, FaFileAlt } from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar = () => {
--- src/pages/Works.js
+++ src/pages/Works.js
@@ -3,16 +3,18 @@ import './Works.css';
 
 const Works = () => {
   const projects = [
-    { title: "Kaizen", link: "/works/project3", description: "Reinventing healthcare architecture." },
-    { title: "Digital Air Rights", link: "/works/project2", description: "Claim. Protect. Prosper." },
-    { title: "Omaha Jazz Festival Poster Redesign", link: "/works/project1", description: "A modern redesign of the Omaha Jazz Fest posters utilizing vibrant colors to capture the energy and spirit of the event." },
+    { title: "Kaizen", description: "Reinventing healthcare architecture.", image: "/kaizen.png" },
+    { title: "Digital Air Rights", description: "Claim. Protect. Prosper.", image: "/DAR.png" },
+    { title: "Omaha Jazz Festival Poster Redesign", link: "/works/project1", description: "A modern redesign of the Omaha Jazz Fest posters utilizing vibrant colors to capture the energy and spirit of the event.", image: "/jazz-festival-image.png" },
   ];
 
   return (
     <div className="works">
       {projects.map((project, index) => (
         <a href={project.link} key={index} className="project-card">
-          <div className="card-header"></div>
+          <div className="card-header">
+            <img src={project.image} alt={project.title} className="project-image" />
+          </div>
           <h2 className="project-title">{project.title}</h2>
           <p className="project-description">{project.description}</p>
         </a>
@@ -22,3 +24,4 @@ const Works = () => {
 };
 
 export default Works;
+
